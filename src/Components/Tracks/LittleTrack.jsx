95a019dc77e12931eef9395c0712030cd4425a30
Percival234 <abaduyaprise@gmail.com>
2024-02-13 13:50:09 +0200

    change little track visibility

--- src/Components/Tracks/LittleTrack.jsx
+++ src/Components/Tracks/LittleTrack.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { twMerge } from 'tailwind-merge';
+import { useSelector } from 'react-redux';
 
 import LinkUnderline from '@UI/Links/LinkUnderline';
 import PlayButton from '@Components/Buttons/PlayButton';
@@ -6,8 +8,15 @@ import ArtistsList from '@Components/Artist/ArtistsRow';
 import LikeButton from '@Components/Buttons/LikeButton';
 
 function LittleTrack({ data: { _id, name, artists }, index, tracks }) {
+  const track = useSelector((state) => state.player.track);
+
+  const classes = twMerge(
+    'relative flex items-center gap-1 p-1 rounded-md duration-200 dark:hover:bg-gray-opacity hover:bg-zinc-50 hover:shadow-md',
+    _id === track?._id ? 'bg-zinc-50 dark:bg-gray-opacity' : ''
+  );
+
   return (
-    <div className="relative flex items-center gap-1 p-1 rounded-md duration-200 dark:hover:bg-gray-opacity hover:bg-zinc-50 hover:shadow-md">
+    <div className={classes}>
       <PlayButton tracks={tracks} index={index} />
       <div className="flex flex-col flex-1 whitespace-nowrap">
         <div className="inline">
