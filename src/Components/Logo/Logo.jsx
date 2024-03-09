53ce24351eadd3fad8a2820321dc69793b7b6bc7
Percival234 <abaduyaprise@gmail.com>
2024-03-09 17:20:33 +0200

    change logo link 2

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
-import NavLinkScroll from '@UI/Links/NavLinkScroll';
+import LinkScroll from '@UI/Links/LinkScroll';
 
 import { useAppContext } from '@Providers/AppProvider';
 
 export default function Logo() {
   const { handleMenuVisibility } = useAppContext();
   return (
-    <NavLinkScroll onClick={handleMenuVisibility} to="/" className="text-4xl text-center font-bold">
+    <LinkScroll onClick={handleMenuVisibility} to="/" className="text-4xl text-center font-bold">
       Soundine
-    </NavLinkScroll>
+    </LinkScroll>
   );
 }
