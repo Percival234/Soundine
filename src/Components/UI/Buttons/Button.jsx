d6352b91d52679248c8046368ca0b3da0b5cebc8
Percival234 <abaduyaprise@gmail.com>
2024-03-09 17:44:29 +0200

    change button bg

--- src/Components/UI/Buttons/Button.jsx
+++ src/Components/UI/Buttons/Button.jsx
@@ -3,7 +3,7 @@ import { twMerge } from 'tailwind-merge';
 
 function Button({ children, className, type, ...props }) {
   const classes = twMerge(
-    'py-2 px-3 lg:px-5 text-sm lg:text-base text-white rounded bg-main border border-solid border-main duration-200 hover:bg-transparent hover:text-main disabled:opacity-50',
+    'py-2 px-3 lg:px-5 text-sm lg:text-base text-white rounded bg-gradient-to-tr from-purple-800 to-main border border-solid border-main duration-200 hover:bg-transparent hover:text-main disabled:opacity-50',
     className
   );
 
