8b4664c1dfbcc3a992a546c8ba73b8a9dff75545
Percival234 <abaduyaprise@gmail.com>
2024-03-13 13:29:19 +0200

    change like logic

--- src/Components/Buttons/LikeButton.jsx
+++ src/Components/Buttons/LikeButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
@@ -8,11 +9,15 @@ import { useToggleFavoritesMutation } from '@Redux/APIEndpoints/userEndpoints';
 
 function LikeButton({ id, type, className }) {
   const user = useSelector((state) => state.user.user);
+  const [isFavorite, setFavorite] = useState(
+    user?.favorites?.[type].some((item) => item._id === id || false)
+  );
   const [toggleFavorites] = useToggleFavoritesMutation();
 
-  const changeFav = () => toggleFavorites({ id, type });
-
-  const isFavorite = user?.favorites?.[type].some((item) => item._id === id);
+  const changeFav = () => {
+    toggleFavorites({ id, type });
+    setFavorite(!isFavorite);
+  };
 
   return (
     <ButtonRounded aria-label="add to favorites" onClick={changeFav} className={className}>
